f60a4eb15aa68d6903913b8db57111eab818ac53
DaleIce <olleti24938@masseyhigh.school.nz>
2026-02-18 12:25:56 +1300

    incomplete HIstory GUI (has calc_list)
    incomplete HIstory GUI (has calc_list)

---
+++ C_04_History_GUI_v2.py
@@ -0,0 +1,118 @@
+from tkinter import  *
+from functools import partial # to prevent unwanted windows
+import all_constants as c
+
+class Converter:
+    """temperature conversion tool (°C to °F or °F to °C)
+     """
+
+    def __init__(self):
+
+
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        # THE History GUI button code
+        self.to_history_button = Button(self.temp_frame,
+                                     text="History / Export",
+                                     bg="#CC6600",
+                                     fg="#FFFFFF",
+                                     font=("Arial", 14, "bold"), width=12,
+                                     command=self.to_history)
+        self.to_history_button.grid(row=1, padx=5, pady=5)
+        # displays the print statement when button is clicked
+
+
+    def to_history(self):
+        """Opens history dialogue box and disables history button
+        (so that users can't create multiple history boxes"""
+        HistoryExport(self, self.all_calculations_list)
+
+class HistoryExport:
+    """
+    Displays history dialogue box
+    """
+
+
+    def __int__(self, partner, calculations):
+
+
+        self.history_box = Toplevel()
+
+        #disable history button
+        partner.to_history_button.config(state=DISABLED)
+
+        # if users press cross at top, closes history and
+        # 'releases' history button
+        self.history_box.protocol('WM_DELETE_WINDOW',
+                                  partial(self.close_history, partner))
+
+        self.history_frame = Frame(self.history_box)
+        self.history_frame.grid()
+
+        # background colour and text for calculation area
+        if len(calculations) <= c.MAX_CALCS:
+            calc_back = "#D5E8D4"
+            calc_amount = "all your"
+        else:
+
+            calc_back = "#ffe6cc"
+            calc_amount = (f"your recent calculations -"
+                           f"showing {c.MAX_CALCS} / {len(calculations)}")
+
+        # stings for 'long' labels...
+        recent_intro_txt = (f"Below are {calc_amount} calculations "
+                            f"(to the nearest degree). ")
+
+        export_instruction_txt = ("Please push <Export> to save your calculations in"
+                                  "file. IF the filename already exist, it will be")
+
+
+
+        # Label list (label text | format | ba)
+        history_label_list = [
+            ["History / Export", ("Arial",16 , "bold"), None],
+            [recent_intro_txt, ("Arial", 11,),None],
+            ["calculation list",("Arial", 14), calc_back],
+            [export_instruction_txt, ("Arial", 11), None]
+        ]
+
+        history_label_ref = []
+        for count, item in enumerate(history_label_list):
+            make_label = Label(self.history_box, text=item[0], font=item[1],
+                               bg=item[2],
+                               wraplength=300, justify="left", pady=10, padx=20)
+            make_label.grid(row=count)
+
+            history_label_ref.append(make_label)
+
+        # Retrieve export instruction label so that we can
+        # configure it to show the filename if the user exports the file
+        self.export_filename_label = history_label_ref[3]
+
+        # make frame to hold buttons (two columns)
+        self.hist_button_frame = Frame(self.history_box)
+        self.hist_button_frame.grid(row=4)
+
+        button_ref_list = []
+
+        # button list (button text | bg colour | command | row | column)
+        button_details_list = [
+            ["Export", "#004C99", "", 0, 0],
+            ["Close", "#666666", partial(self.close_history, partner), 0, 1],
+        ]
+
+        for btn in button_details_list:
+            self.make_button = Button(self.hist_button_frame,
+                                      font=("Arial", 12, "bold"),
+                                      text=btn[0], bg=btn[1],
+                                      fg="#FFFFFF", width=12,
+                                      command=btn[2])
+        self.make_button.grid(row=btn[3], column=btn[4], padx=10, pady=10)
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
+
